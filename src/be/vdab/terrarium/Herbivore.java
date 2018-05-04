48bc47c279e91c9f76d8f7e6115eebce88dc23ee
Bianca Reymen <Bianca.Reymen@pandora.be>
2018-05-04 15:11:05 +0200

    Herbivore gecorrigeerd

--- src/be/vdab/terrarium/Herbivore.java
+++ src/be/vdab/terrarium/Herbivore.java
@@ -1,16 +1,16 @@
 package be.vdab.terrarium;
 
-public class Herbivore implements Organism {
+public class Herbivore extends Animal {
 
 	public Herbivore () {
-		
+		this.lifeForce = 0;
 	}
-	
 	
 	@Override
 	public char draw() {
 		return 'H';
 	}
+	
 	public Herbivore sex() {
 		return new Herbivore();
 	}
