885da732da0e91764d22d2305ca4a53da1c75309
Jimmy Godin <jimmy.godin@hotmail.com>
2018-05-07 11:20:08 +0200

    remove lifeforce in animal

--- src/be/vdab/terrarium/Animal.java
+++ src/be/vdab/terrarium/Animal.java
@@ -1,20 +1,12 @@
 package be.vdab.terrarium;
 
 public class Animal extends Organism {
-	int lifeforce = 0;
+	
 	
 	public Animal(Coordinate coordinate) {
 		super(coordinate);
 	}
 	
-	public int getLifeforce() {
-		return lifeforce;
-	}
-
-	public void setLifeforce(int lifeforce) {
-		this.lifeforce = lifeforce;
-	}
-
 	@Override
 	public char draw() {
 		return 0;
